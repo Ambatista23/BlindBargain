f3b3dd79a513f4e99fbb37d52f847b5654325ca7
Ismael Rivera <i.rvra130@gmail.com>
2019-07-27 11:59:55 -0400

    update on components

--- blindbargin/src/App.js
+++ blindbargin/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Nav from "../../blindbargin/src/components/Nav";
 import Jumbotron from "../../blindbargin/src/components/Jumbotron";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Menu from "../../blindbargin/src/components/Demo";
+import MenuItem from "../../blindbargin/src/components/Demo";
+import Demo from "../../blindbargin/src/components/Demo";
 import './App.css';
+import PrimarySearchAppBar from '../../blindbargin/src/components/Demo';
 
 function App() {
 
   return (
     <Router>
       <div>
-        <Nav />
+        <Demo>
+          {PrimarySearchAppBar}
+        <Menu>
+          <MenuItem />
+          </Menu> />
+          </Demo>
+        
+        
         <Jumbotron />
       </div>
     </Router>
--- blindbargin/src/components/Jumbotron/index.js
+++ blindbargin/src/components/Jumbotron/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >somthing to see
+      style={{ height: 220, clear: "both", paddingTop: 80, textAlign: "center" }}
+      className="jumbotron">
+        
+        <h1>Welcome to Blind Bargin!</h1>
+        <h2>No one runs a better auction.</h2>
       {children}
     </div>
   );
